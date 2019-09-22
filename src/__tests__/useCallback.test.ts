4ed74eac53097b87e01b7b1fb8ab45de62d8d965
shidong.ke <keshidong.dev@gmail.com>
2019-09-22 10:23:09 +0800

    add unit test for useCallback

--- src/__tests__/useCallback.test.ts
+++ src/__tests__/useCallback.test.ts
@@ -1,8 +1,6 @@
 import { renderHook } from '@testing-library/react-hooks'
 import { useCallback } from '..'
 
-const mockCallback = jest.fn()
-
 afterEach(() => {
     jest.resetAllMocks()
 })
@@ -11,7 +9,7 @@ it('should keep same ref after props/state change', () => {
     let cbRef
     const { rerender } = renderHook(
         ({ count }) => {
-            cbRef = useCallback(mockCallback, [count])
+            cbRef = useCallback(() => {}, [count])
         },
         { initialProps: { count: 0 } }
     )
@@ -20,4 +18,17 @@ it('should keep same ref after props/state change', () => {
     rerender({ count: 1 })
 
     expect(cbRef).toBe(ref1)
+})
+
+it('should have same output with React/useCallback after props/state change', () => {
+    let frezzeRefCb
+    const { rerender } = renderHook(
+        ({ count }) => {
+            frezzeRefCb = useCallback(() => count, [count])
+        },
+        { initialProps: { count: 0 } }
+    )
+    expect(frezzeRefCb()).toEqual(0)
+    rerender({ count: 1 })
+    expect(frezzeRefCb()).toEqual(1)
 })
